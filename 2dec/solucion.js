b9dc4450f1e7d7a135e8e7fc44bd74fd113d057d
Carlos Ajenjo <carlosajenjo@gmail.com>
2022-12-02 19:20:09 +0100

    add second challenge

---
+++ 2dec/solucion.js
@@ -0,0 +1,19 @@
+console.clear();
+
+const year = 2023;
+const holidays = ['01/06', '04/01', '12/25'];
+
+function countHours(year, holidays) {
+  const weeksDays = [1, 2, 3, 4, 5];
+  const date = new Date();
+  let hours = 0;
+
+  for (let holiday of holidays) {
+    date.setTime(Date.parse(holiday + '/' + year));
+    if (weeksDays.includes(date.getDay())) {
+      hours += 2;
+    }
+  }
+  return hours;
+}
+console.log(countHours(year, holidays));
