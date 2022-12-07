36c13a85efaea8515829cd6367ecd3b9b1433028
Carlos Ajenjo <carlosajenjo@gmail.com>
2022-12-07 22:07:36 +0100

    addadd challenge nº7

--- 7dec/solucion.js
+++ 7dec/solucion.js
@@ -16,10 +16,10 @@ function getGiftsToRefill(a1, a2, a3) {
   // Ahora le podemos pasar un filter a este nuevo array
   // y miramos a ver si el actual elemento se encuentra en los arrays originales, con includes()
   // En caso de existir al menos un elemento, nos devolvera true, en caso contrario un false.
-  // Podemos sumar los 3 trues, que se le da valor numerico de 1, entonces si la suma de los tres es igual a 1, devuelve el elemento, en caso contrario no lo devuelve, dejando asi, un filtrado del array que hemos creado solo con los elementos que no se repiten mas de una vez.
+  // Podemos sumar los 3 trues, que se le da valor numerico de 1, entonces si la suma de los tres es inferior a 2, devuelve el elemento, en caso contrario no lo devuelve, dejando asi, un filtrado del array que hemos creado solo con los elementos que no se repiten mas de una vez.
 
   return totalGifts.filter(
-    (gift) => a1.includes(gift) + a2.includes(gift) + a3.includes(gift) === 1
+    (gift) => a1.includes(gift) + a2.includes(gift) + a3.includes(gift) < 2
   );
 }
 
